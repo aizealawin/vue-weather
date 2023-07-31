72d3430698118065b71e05416a5dd47303115609
aizealawin <ktmudalige@gmail.com>
2023-07-31 13:41:47 -0400

    axios call for current weather

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <div id="app"></div>>
+  <div id="app">Hello</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+const API_KEY = process.env.VUE_APP_WEATHER_KEY
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: () => ({
+    dailyWeather: [],
+    currentWeather: null
+  }),
+  methods: {
+    async getCurrentWeather(coords) {
+      const res = await axios.get(
+        `
+        https://api.openweathermap.org/data/2.5/onecall?lat=${coords.latitude}&lon=${coords.longitude}&units=imperial&appid=${API_KEY}
+        `
+      )
+      console.log(res.data)
+      this.currentWeather = res.data.current
+    }
   }
 }
 </script>
